12ec640fcb1fa0135826165da0e247af1e57fe63
Ryan Stanley <stanleyrya@gmail.com>
2020-11-19 18:40:39 -0800

    iterating on nearby wiki articles

--- widget.js
+++ widget.js
@@ -85,6 +85,9 @@ async function getMapsPicByCity(apiKey, city) {
  * 
  * Example output from Wikipedia:
  * {"query":{"geosearch":[{"pageid":38743,"primary":"","dist":432.1,"ns":0,"title":"Cape Cod","lon":-70.2,"lat":41.68},{"pageid":4866309,"primary":"","dist":721.7,"ns":0,"title":"Cape Cod Coliseum","lon":-70.19622222222223,"lat":41.68996944444444},{"pageid":3051164,"primary":"","dist":738,"ns":0,"title":"Dennis-Yarmouth Regional High School","lon":-70.19363611111112,"lat":41.677974999999996},{"pageid":39230827,"primary":"","dist":779.3,"ns":0,"title":"Red Wilson Field","lon":-70.194,"lat":41.6774},{"pageid":2013195,"primary":"","dist":1120.2,"ns":0,"title":"WKPE-FM","lon":-70.189,"lat":41.691},{"pageid":58211602,"primary":"","dist":1549.7,"ns":0,"title":"Long Pond (West Yarmouth, Massachusetts)","lon":-70.1971508,"lat":41.669742},{"pageid":39494986,"primary":"","dist":1691.5,"ns":0,"title":"Yarmouth-Barnstable Regional Transfer Station","lon":-70.21765833333333,"lat":41.68823333333333},{"pageid":116666,"primary":"","dist":1769.8,"ns":0,"title":"South Yarmouth, Massachusetts","lon":-70.19972222222222,"lat":41.66777777777777},{"pageid":21797930,"primary":"","dist":1841.8,"ns":0,"title":"Dennis Beaches","lon":-70.17791666666668,"lat":41.67701388888889},{"pageid":18446199,"primary":"","dist":2105.4,"ns":0,"title":"Thomas Bray Farm","lon":-70.20805555555556,"lat":41.70111111111111}]},"batchcomplete":""}
+ * 
+ * Example output:
+ * [{"url":"https://en.wikipedia.org/?38743","primary":"","distance":432.1,"ns":0,"title":"Cape Cod","lng":-70.2,"lat":41.68}]
  */
 async function getNearbyWikiArticles(lat, lng) {
 	try {
@@ -92,7 +95,28 @@ async function getNearbyWikiArticles(lat, lng) {
 		console.log('Request URI: ' + uri);
 		const request = new Request(encodeURI(uri));
 		const wikiJSON = await request.loadJSON();
-		console.log(wikiJSON);
+		console.log('Wiki JSON: ' + wikiJSON);
+
+		let articles;
+		if (wikiJSON && wikiJSON.query && wikiJSON.query.geosearch) {
+			articles = wikiJSON.query.geosearch;
+		} else {
+			throw new Error("Could not read data from wikipedia");
+		}
+		console.log(articles);
+
+		const response = [];
+// 		for (int i=0; i<articles; i++) {
+// 			response.push({
+// 				"url":"https://en.wikipedia.org/?" + articles[i].pageid,
+// 				"primary":"",
+// 				"distance":articles[i].dist,
+// 				"ns":0,
+// 				"title":"Cape Cod",
+// 				"lng":-70.2,
+// 				"lat":41.68
+// 			})
+// 		}
 	} catch(e) {
 		console.error(e)
 		return null;
