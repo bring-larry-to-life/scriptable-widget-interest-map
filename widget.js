5c65cf90bfe03b75eeb48a4fe3442680738e6c1a
Ryan Stanley <stanleyrya@gmail.com>
2020-11-19 20:04:39 -0800

    Adding comments for new wikipedia flow, will add flow in a sec

--- widget.js
+++ widget.js
@@ -98,12 +98,26 @@ async function getMapsPicByCity(apiKey, city) {
 
 /*
  * Calls wikipedia for nearby articles and modifies the object for ease of use.
+ *
+ * Example Wikipedia API:
+ * https://en.wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cpageimages&generator=geosearch&ggscoord=41.68365535753726|-70.19823287890266&ggsradius=10000
+ * 
+ * Useful Wikipedia article on how to use API with location and images:
+ * https://www.mediawiki.org/wiki/API:Geosearch#Example_3:_Search_for_pages_nearby_with_images
+ *
+ * Useful StackOverflow article about using Wikipedia API with location and images:
+ * https://stackoverflow.com/questions/24529853/how-to-get-more-info-within-only-one-geosearch-call-via-wikipedia-api
  * 
  * Example output from Wikipedia:
- * {"query":{"geosearch":[{"pageid":38743,"primary":"","dist":432.1,"ns":0,"title":"Cape Cod","lon":-70.2,"lat":41.68},{"pageid":4866309,"primary":"","dist":721.7,"ns":0,"title":"Cape Cod Coliseum","lon":-70.19622222222223,"lat":41.68996944444444},{"pageid":3051164,"primary":"","dist":738,"ns":0,"title":"Dennis-Yarmouth Regional High School","lon":-70.19363611111112,"lat":41.677974999999996},{"pageid":39230827,"primary":"","dist":779.3,"ns":0,"title":"Red Wilson Field","lon":-70.194,"lat":41.6774},{"pageid":2013195,"primary":"","dist":1120.2,"ns":0,"title":"WKPE-FM","lon":-70.189,"lat":41.691},{"pageid":58211602,"primary":"","dist":1549.7,"ns":0,"title":"Long Pond (West Yarmouth, Massachusetts)","lon":-70.1971508,"lat":41.669742},{"pageid":39494986,"primary":"","dist":1691.5,"ns":0,"title":"Yarmouth-Barnstable Regional Transfer Station","lon":-70.21765833333333,"lat":41.68823333333333},{"pageid":116666,"primary":"","dist":1769.8,"ns":0,"title":"South Yarmouth, Massachusetts","lon":-70.19972222222222,"lat":41.66777777777777},{"pageid":21797930,"primary":"","dist":1841.8,"ns":0,"title":"Dennis Beaches","lon":-70.17791666666668,"lat":41.67701388888889},{"pageid":18446199,"primary":"","dist":2105.4,"ns":0,"title":"Thomas Bray Farm","lon":-70.20805555555556,"lat":41.70111111111111}]},"batchcomplete":""}
+ * {"batchcomplete":"","query":{"pages":{"38743":{"pageid":38743,"ns":0,"title":"Cape Cod","index":-1,"coordinates":[{"lat":41.68,"lon":-70.2,"primary":"","globe":"earth"}],"thumbnail":{"source":"https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Ccnatsea.jpg/50px-Ccnatsea.jpg","width":50,"height":34},"pageimage":"Ccnatsea.jpg"}}}}
  * 
- * Example output:
- * [{"url":"https://en.wikipedia.org/?38743","primary":"","distance":432.1,"ns":0,"title":"Cape Cod","lng":-70.2,"lat":41.68}]
+ * Example output: [{
+ *   lat: 41.68
+ *   lng: -70.2
+ *   thumbnail: {source: "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Ccnatsea.jpg/50px-Ccnatsea.jpg", width: 50, height: 34}
+ *   title: "Cape Cod",
+ *   url: https://en.wikipedia.org/?38743
+ * }]
  */
 async function getNearbyWikiArticles(lat, lng) {
 	try {
