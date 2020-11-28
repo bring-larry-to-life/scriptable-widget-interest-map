05acaf73d3bea61b6e87d0d00196ee4ffdcb7ef8
Ryan Stanley <stanleyrya@gmail.com>
2020-11-28 16:07:08 -0500

    Tried to load stored parameters if other parameters not provided

--- widget.js
+++ widget.js
@@ -19,6 +19,44 @@ const performanceResultsInMillis = {};
  ****** UTILITY FUNCTIONS ******
  *******************************/
 
+/**
+ * Attempts to load the file ./storage/filename.json
+ * Returns null if it cannot be loaded.
+ */
+function loadStoredParameters() {
+    let fm = FileManager.local()
+
+    const scriptPath = module.filename
+    const storageDir = scriptPath.replace(fm.fileName(scriptPath, true), '') + "storage"
+    const parameterFile = Script.name() + ".json";
+    const parameterPath = storageDir + "/" + parameterFile;
+
+    if (!fm.fileExists(storageDir)) {
+        console.log("Storage folder does not exist!");
+        return null;
+    }
+
+    if (fm.fileExists(storageDir) && !fm.isDirectory(storageDir)) {
+        console.log("Storage folder exists but is not a directory!");
+        return null;
+    }
+
+    if (!fm.fileExists(parameterPath)) {
+        console.log("Parameter file does not exist!");
+        return null;
+    }
+    
+    const parameterString = fm.readString(parameterPath);
+
+    const parameterJSON = JSON.parse(parameterString)
+    if (parameterJSON !== null) {
+        return parameterJSON;
+    } else {
+        console.log("Could not load parameter file as JSON!");
+        return null;
+    }
+}
+
  const performanceWrapper = async (fn, args) => {
 	const start = Date.now();
 	const result = await fn.apply(null, args);
@@ -410,12 +448,14 @@ async function run(params) {
         let params;
         if (defaultParams) {
             params = defaultParams;
-        } else {
+        } else  if (args.widgetParameter) {
             params = JSON.parse(args.widgetParameter);
+        } else {
+            params = loadStoredParameters();
         }
-        console.log(params)
 		
 		if (params) {
+            console.log("Using params: " + JSON.stringify(params))
 			await run(params);
 		} else {
 			console.log("No valid parameters!")
